945f28c98eb265af6cda31563d9cc737e16ea971
Arkadiy Korotaev <koriaf@gmail.com>
2020-07-04 16:12:05 +0200

    fix(subscr): Local improvements for celery beat being available to access IGL api (for CELERY_ALWAYS_EAGER turned on)

--- trade_portal/trade_portal/websub_receiver/management/commands/resubscribe.py
+++ trade_portal/trade_portal/websub_receiver/management/commands/resubscribe.py
@@ -1,10 +1,10 @@
 from django.core.management.base import BaseCommand
 
-from trade_portal.websub_receiver.tasks import subscribe_to_notifications
+from trade_portal.websub_receiver.tasks import subscribe_to_new_messages
 
 
 class Command(BaseCommand):
     help = 'Re-subscribe to all incoming messages from a node'
 
     def handle(self, *args, **kwargs):
-        subscribe_to_notifications()
+        subscribe_to_new_messages()
