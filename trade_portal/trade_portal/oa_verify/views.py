9514144feb099a80221ab30cc0cde75ff523426e
Arkadiy Korotaev <koriaf@gmail.com>
2020-08-25 18:39:46 +0200

    feat(verifier): Show metadata tab for verified documents

--- trade_portal/trade_portal/oa_verify/views.py
+++ trade_portal/trade_portal/oa_verify/views.py
@@ -1,18 +1,25 @@
+import json
+import logging
+
 import requests
 
 from constance import config
 from django.views.generic import TemplateView
+
+logger = logging.getLogger(__name__)
 
 
 class OaVerificationView(TemplateView):
     template_name = "oa_verify/verification.html"
     verify_result = None
     verdict = None
+    metadata = None
 
     def get_context_data(self, *args, **kwargs):
         c = super().get_context_data(*args, **kwargs)
         c['verify_result'] = self.verify_result
         c['verdict'] = self.verdict
+        c['metadata'] = self.metadata
         return c
 
     def post(self, request, *args, **kwargs):
@@ -26,7 +33,51 @@ class OaVerificationView(TemplateView):
                     self.verdict = "invalid"
         else:
             self.verdict = "error"
+
+        if self.verdict == "valid":
+            try:
+                unwrapped_file = self._unwrap_file(value)
+            except Exception as e:
+                logger.exception(e)
+            else:
+                self.metadata = self._retrtieve_metadata(unwrapped_file)
+
         return super().get(request, *args, **kwargs)
+
+    def _retrtieve_metadata(self, data):
+        md = data["data"].copy()
+        md.pop("attachments", None)
+        return md
+
+    def _unwrap_file(self, content):
+        """
+        Warning: it's unreliable but quick
+        """
+        def unwrap_it(what):
+            if isinstance(what, str):
+                # wrapped something
+                if what.count(":") >= 2:
+                    uuidvalue, vtype, val = what.split(":", maxsplit=2)
+                    if len(uuidvalue) == len("6cdb27f1-a46e-4dea-b1af-3b3faf7d983d"):
+                        if vtype == "string":
+                            return str(val)
+                        elif vtype == "boolean":
+                            return True if val.lower() == "true" else False
+                        elif vtype == "number":
+                            return int(val)
+                    else:
+                        return what
+            elif isinstance(what, dict):
+                return {
+                    k: unwrap_it(v)
+                    for k, v
+                    in what.items()
+                }
+            else:
+                return what
+
+        wrapped = json.loads(content)
+        return unwrap_it(wrapped)
 
     def _verify_file(self, file_content):
         resp = requests.post(
