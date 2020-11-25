8994ea126a7a0ce8f5d0fbfa9c997add462a06d4
Arkadiy Korotaev <koriaf@gmail.com>
2020-11-25 14:32:54 +0100

    feat(subscriptions): Schedule initial subscribe on the celery-beat init instead of app init

--- trade_portal/config/celery_app.py
+++ trade_portal/config/celery_app.py
@@ -1,5 +1,6 @@
 import os
 from celery import Celery
+from celery.signals import beat_init
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
@@ -14,3 +15,9 @@ app.config_from_object("django.conf:settings", namespace="CELERY")
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
+
+
+@beat_init.connect()
+def on_startup_subscribe(conf=None, **kwargs):
+    from trade_portal.websub_receiver.tasks import subscribe_to_new_messages
+    subscribe_to_new_messages.delay()
--- trade_portal/trade_portal/websub_receiver/apps.py
+++ trade_portal/trade_portal/websub_receiver/apps.py
@@ -1,7 +1,6 @@
 import logging
 
 from django.apps import AppConfig
-from django.conf import settings
 
 logger = logging.getLogger(__name__)
 
@@ -10,11 +9,3 @@ class WebSubReceiverAppConfig(AppConfig):
 
     name = "trade_portal.websub_receiver"
     verbose_name = "WebSub Receiver module"
-
-    def ready(self):
-        is_unittest = getattr(settings, "IS_UNITTEST", False)
-        is_debug = settings.DEBUG
-        if is_debug and not is_unittest:
-            from trade_portal.websub_receiver.tasks import subscribe_to_new_messages
-
-            subscribe_to_new_messages()
