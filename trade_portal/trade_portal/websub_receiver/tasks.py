8358203b79f56cf23a97b5f2b669c6f2a10efa45
Adam Kerz <adam.kerz@gosource.com.au>
2021-02-05 17:33:02 +1000

    Disable subscribe task

--- trade_portal/trade_portal/websub_receiver/tasks.py
+++ trade_portal/trade_portal/websub_receiver/tasks.py
@@ -6,7 +6,7 @@ from config import celery_app as app
 logger = logging.getLogger(__name__)
 
 
-@app.task(ignore_result=True, max_retries=3)
+#@app.task(ignore_result=True, max_retries=3)
 def subscribe_to_new_messages():
     try:
         NodeService().subscribe_to_new_messages()
